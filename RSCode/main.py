6e0306d28db4228be03fc57b161d4a93e6de29ed
Дамиров Намиг <damirovnamig@MacBook-Pro-Damirov.local>
2020-10-10 14:22:54 +0300

    Getting positions of errors

--- RSCode/main.py
+++ RSCode/main.py
@@ -24,7 +24,65 @@ def encode(msg, add):
 # counts syndrome polynom
 # must be 0 at first <add> values: a^i, a = 2
 def syndrome(msg, add):
-    synd = [0] * (add + 1)
+    # synd = [0] * (add + 1)
+    # for i in range(add):
+    #     synd[i + 1] = pol_get(msg, f_pow(2, i))
+    synd = [0] * add
     for i in range(add):
-        synd[i + 1] = pol_get(msg, f_pow(2, i))
+        synd[i] = pol_get(msg, f_pow(2, i))
     return synd
+
+# checks if message is corrupted
+def check(msg, add):
+    # values of syndrome polynome can't be < 0
+    # because of GF(2^8)
+    return (max(syndrome(msg, add)) == 0)
+
+# counts error locator polynom
+# Berlekamp-Messey algorithm
+# roots of locator polynom are errors
+def error_locator(synd):
+    err = [1] # locator polynom
+    prev = [1] # previous state for algorithm
+    add = len(synd) # number of added symbols
+
+    for i in range(add): 
+        delta = synd[i]        
+        for j in range(1, len(err)):
+            delta ^= multiply(err[-j - 1], synd[i - j])
+        prev += [0]
+        
+        if delta == 0:
+            continue
+
+        if len(prev) > len(err):
+            err, prev = pol_scale(prev, delta), \
+                        pol_scale(err, inverse(delta))
+        
+        err = pol_add(err, pol_scale(prev, delta))
+    
+    while len(err) > 0 and err[0] == 0: 
+        del err[0]
+    amount = len(err) - 1
+    if amount * 2 > add:
+        raise Exception()
+
+    return err
+
+# find errors using locator polynom
+# simple brutforce
+def find(loc, msl): # msl = len(msg_in) = len(msg) - add
+    amount = len(loc) - 1
+    positions = []
+    for i in range(msl):
+        if pol_get(loc, f_pow(2, i)) == 0:
+            positions += [msl - i - 1]
+    
+    if len(positions) != amount:
+        raise Exception()
+
+    return positions
+
+# Forney's algorithm
+def fix():
+    pass
