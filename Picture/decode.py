fabb3dd95c9c9f4a37278da60fbfdda9fb9aa5d8
Дамиров Намиг <damirovnamig@MacBook-Pro-Damirov.local>
2020-10-13 10:12:01 +0300

    Better decoding

--- Picture/decode.py
+++ Picture/decode.py
@@ -1,9 +1,10 @@
 from PIL import Image
+
 def decode(name='temp.png'):
     img = Image.open(name)
     (w, h) = img.size
 
-    # get size of (i, j) square
+    # gives average color of (i, j) square
     def get_pixel(i, j):
         nonlocal img, w, h
         amount = 0
@@ -18,11 +19,10 @@ def decode(name='temp.png'):
         return (r // amount, g // amount, b // amount)
 
     # searches for closest color
-    def find(x, y):
+    def find(col):
         nonlocal t
-        col = get_pixel(x, y)
         mn = 255 * 3
-        res = 1
+        res = 0
         for i in range(4):
             tmp = 0
             for j in range(3):
@@ -30,6 +30,20 @@ def decode(name='temp.png'):
             if tmp < mn:
                 res = i
                 mn = tmp
+
+        return res
+    
+    # searches for closest color of square
+    def find_square(i, j):
+        nonlocal img, w, h
+        t = [0] * 4
+        for x in range(w * i // 23, w * (i + 1) // 23):
+            for y in range(h * j // 23, h * (j + 1) // 23):
+                t[find(img.getpixel((x, y)))] += 1
+        res = 0
+        for i in range(1, 4):
+            if t[i] > t[res]:
+                res = i
 
         return res
 
@@ -53,7 +67,7 @@ def decode(name='temp.png'):
     
     for i in range(23):
         for j in range(23):
-            arr[i][j] = find(i, j)
+            arr[i][j] = find_square(i, j)
 
     length = 0
     length ^= arr[7][-8 - 1] ^ arr[-8 - 1][-8 - 1] ^ arr[-8 - 1][7]
