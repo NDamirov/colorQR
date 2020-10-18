186b251602b3627892a32a32df0a27a4e6cfc233
Дамиров Намиг <damirovnamig@MacBook-Pro-Damirov.local>
2020-10-18 15:24:16 +0300

    Bug fixes

--- Picture/detect.py
+++ Picture/detect.py
@@ -5,21 +5,22 @@ def detect(name="temp.png"):
     (w, h) = img.size
 
     # constants
-    col_similar = 20
-    delta = 0.1
+    col_similar = 60
+    delta = 0.5
 
     def get(i, j):
         nonlocal img
         return img.getpixel((i, j))
 
     def similar(col1, col2):
+        sm = 0
         for i in range(3):
-            if abs(col1[i] - col2[i]) > col_similar:
-                return False
-        return True
+            sm += abs(col1[i] - col2[i])
+        return (sm < col_similar)
     
     def find(ratio):
-        for col in range(w):
+        nonlocal img
+        for col in range(0, w):
             temp_groups = []
             last = (-100, -100, -100)
             for row in range(h):
@@ -31,9 +32,13 @@ def detect(name="temp.png"):
                 last = curr
             
             groups = []
+            gr_start = []
+            curr = 0
             for g in temp_groups:
                 if g[1] > 3:
                     groups.append(g)
+                    gr_start.append(curr)
+                curr += g[1]
 
             for i in range(len(groups) - 5):
                 if not (similar(groups[i][0], groups[i + 2][0]) and
@@ -42,12 +47,12 @@ def detect(name="temp.png"):
                         similar(groups[i + 1][0], groups[i + 5][0])):
                     continue
                 
-                if not (abs(groups[i][1] / groups[i + 1][1] - ratio[0] / ratio[1]) < delta or 
-                        abs(groups[i + 1][1] / groups[i + 2][1] - ratio[1] / ratio[2]) < delta or
-                        abs(groups[i + 2][1] / groups[i + 3][1] - ratio[2] / ratio[3]) < delta or
-                        abs(groups[i + 3][1] / groups[i + 4][1] - ratio[3] / ratio[4]) < delta):
+                if not (abs(groups[i][1] / groups[i + 1][1] - ratio[0] / ratio[1]) < delta and 
+                        abs(groups[i + 1][1] / groups[i + 2][1] - ratio[1] / ratio[2]) < delta and
+                        abs(groups[i + 2][1] / groups[i + 3][1] - ratio[2] / ratio[3]) < delta and
+                        abs(groups[i + 3][1] / groups[i + 4][1] - ratio[3] / ratio[4]) < delta and
+                        abs(groups[i + 4][1] / groups[i + 5][1] - ratio[4] / ratio[5]) < delta):
                     continue
-                for t in range(i, i + 5):
-                    print(groups[t])
-                return
+                img.putpixel((col, gr_start[i + 2] + groups[i + 2][1] // 2), (0, 0, 0))
     find([1, 1, 3, 1, 1, 1])
+    img.save("decoded.png")
